4b99033508b3884120c66f4aa208ca78073c6b83
Evan Turner <evturn@gmail.com>
2015-03-01 09:11:08 -0500

    second child view created

--- js/backbone/child_views.js
+++ js/backbone/child_views.js
@@ -6,3 +6,12 @@ var View1 = Backbone.View.extend({
 		this.$el.html(this.model.get('Message') + ' Sup, I\'m your first view.');
 	},
 });
+
+var View2 = Backbone.View.extend({
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.model.get('Message') + ' I\'m your second');
+	},
+});
