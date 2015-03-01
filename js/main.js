cd14447b2840752948935d3b878f6f12ead1aa7c
Evan Turner <evturn@gmail.com>
2015-03-01 09:20:33 -0500

    instance of parent view created with and el and model

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 var greeting = new GreetModel({Message: "Your View"});
+
+var container = new ContainerView({el: $('#AppContainer'), mode: greeting});
+
+var view1 = null;
+var view2 = null;
+var view3 = null;
