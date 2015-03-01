a4548369d19247b3d439330af746cee31ceda513
Evan Turner <evturn@gmail.com>
2015-03-01 09:12:01 -0500

    three child views created

--- js/backbone/child_views.js
+++ js/backbone/child_views.js
@@ -4,6 +4,7 @@ var View1 = Backbone.View.extend({
 	},
 	render: function() {
 		this.$el.html(this.model.get('Message') + ' Sup, I\'m your first view.');
+		return this;
 	},
 });
 
@@ -13,5 +14,16 @@ var View2 = Backbone.View.extend({
 	},
 	render: function() {
 		this.$el.html(this.model.get('Message') + ' I\'m your second');
+		return this;
+	},
+});
+
+var View3 = Backbone.View.extend({
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.model.get('Message') + ' I AM VIEW THREE');
+		return this;
 	},
 });
